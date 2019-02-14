456bcedb12933ecaf7432075df32b78efa8dac94
Axel <axel.coronel@mercadolibre.com>
2019-02-14 17:25:20 -0300

    algo

--- go-talks/resources/src/01_zero_values/main.go
+++ go-talks/resources/src/01_zero_values/main.go
@@ -6,7 +6,8 @@ var i int
 var f float64
 var b bool
 var s string
+var arr [1]float32
 
 func init() { fmt.Println("Se ejecuta init") }
 
-func main() { fmt.Printf("%v %v %v %q\n", i, f, b, s) }
+func main() { fmt.Printf("%v %v %v %q %v\n", i, f, b, s, arr) }
